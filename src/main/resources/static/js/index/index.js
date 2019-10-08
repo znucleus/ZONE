84a51d01056dec7b8a7ddcc4c3da5bdf946dfb19
zbeboy <863052317@qq.com>
2019-10-08 23:04:06 +0800

    首页

--- src/main/resources/static/js/index/index.js
+++ src/main/resources/static/js/index/index.js
@@ -1,11 +1,33 @@
+/**
+ * Created by zbeboy on 2019-10-07.
+ */
 requirejs.config({
+    map: {
+        '*': {
+            'css': web_path + '/webjars/require-css/css.min.js' // or whatever the path to require-css is
+        }
+    },
+    // pathsオプションの設定。"module/name": "path"を指定します。拡張子（.js）は指定しません。
+    paths: {
+        "wow": web_path + "/plugins/wow/wow.min",
+        "vegas": web_path + "/plugins/vegas/vegas.min",
+        "moment": web_path + "/plugins/moment/moment.min",
+        "bootstrap": web_path + "/plugins/bootstrap/js/bootstrap.bundle.min"
+    },
     // shimオプションの設定。モジュール間の依存関係を定義します。
     shim: {
-        "app": {
-            deps: ["particles"]
+        "wow": {
+            deps: ["jquery", "css!" + web_path + "/plugins/animate/animate.min"]
+        },
+        "vegas": {
+            deps: ["jquery", "css!" + web_path + "/plugins/vegas/vegas.min"]
+        },
+        "bootstrap": {
+            deps: ["jquery"]
         }
     }
 });
+
 /*
  捕获全局错误
  */
@@ -18,19 +40,41 @@ requirejs.onError = function (err) {
 };
 
 // require(["module/name", ...], function(params){ ... });
-require(["jquery", "modernizr.custom.86080", "app"], function ($) {
-    var ajax_url = {
-        login: web_path + '/login'
-    };
+require(["jquery", "bootstrap", "wow", "vegas"], function ($) {
+    // Preloader
+    $('.preloader').fadeOut(1000);
 
-    $('#login').click(function () {
-        window.location.href = ajax_url.login;
+    var body = $('body');
+
+    // qq social
+    $('.fa-qq').popover({
+        trigger: 'focus',
+        html: true,
+        animation: true,
+        content: '<img src="' + web_path + '/images/social-qq.png" style="height: 150px;width: 150px;" alt="QQ群" />',
+        placement: 'top'
     });
 
-    console.log('%c ██████    █      █   █████', 'color:blue');
-    console.log('%c      █    █      █   █   ', 'color:blue');
-    console.log('%c    █      ████████   █████', 'color:red');
-    console.log('%c  █        █      █   █', 'color:red');
-    console.log('%c ██████    █      █   █████', 'color:green');
-    console.log('%c Welcome to Z.核.', 'color:red');
+    // weixin social
+    $('.fa-weixin').popover({
+        trigger: 'focus',
+        html: true,
+        animation: true,
+        content: '<img src="' + web_path + '/images/social-weixin.jpg" style="height: 150px;width: 150px;" alt="微信公众号" />',
+        placement: 'top'
+    });
+
+    // pic
+    body.vegas({
+        slides: [
+            {src: web_path + '/images/slide-3.jpg'},
+            {src: web_path + '/images/slide-4.jpg'}
+        ],
+        timer: false,
+        transition: ['zoomOut']
+    });
+
+    new WOW({mobile: false}).init();
+
+    console.log('%c Welcome to ZONE.', 'color:red');
 });
