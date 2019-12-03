fe3dfd7b1b6350bb4aeba3003df2b1011706e3d4
zbeboy <863052317@qq.com>
2019-12-03 21:48:04 +0800

    优化

--- src/main/java/top/zbeboy/zone/web/system/application/SystemApplicationRestController.java
+++ src/main/java/top/zbeboy/zone/web/system/application/SystemApplicationRestController.java
@@ -34,7 +34,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 @RestController
 public class SystemApplicationRestController {
