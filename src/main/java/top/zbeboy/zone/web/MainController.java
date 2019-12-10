c61daf93c4310b0d4b1b423fb1517f09ef1aa3fc
zbeboy <863052317@qq.com>
2019-12-10 22:57:30 +0800

    去除https

--- src/main/java/top/zbeboy/zone/web/MainController.java
+++ src/main/java/top/zbeboy/zone/web/MainController.java
@@ -1,34 +1,26 @@
 package top.zbeboy.zone.web;
 
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import top.zbeboy.zone.annotation.logging.LoggingRecord;
 import top.zbeboy.zone.config.Workbook;
-import top.zbeboy.zone.config.ZoneProperties;
 import top.zbeboy.zone.domain.tables.pojos.Files;
 import top.zbeboy.zone.domain.tables.pojos.Users;
 import top.zbeboy.zone.service.platform.MenuService;
 import top.zbeboy.zone.service.platform.UsersService;
 import top.zbeboy.zone.service.system.AuthoritiesService;
 import top.zbeboy.zone.service.system.FilesService;
-import top.zbeboy.zone.service.upload.UploadService;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.util.List;
 import java.util.Objects;
 
 @Controller
 public class MainController {
-
-    private final Logger log = LoggerFactory.getLogger(MainController.class);
 
     @Resource
     private AuthoritiesService authoritiesService;
@@ -41,12 +33,6 @@ public class MainController {
 
     @Resource
     private FilesService filesService;
-
-    @Resource
-    private ZoneProperties ZoneProperties;
-
-    @Resource
-    private UploadService uploadService;
 
     /**
      * 登录页
@@ -107,23 +93,5 @@ public class MainController {
         modelMap.addAttribute("menu", menuService.getMenu(roles, users.getUsername()));
 
         return "backstage";
-    }
-
-    /**
-     * let's encrypt certificate check.
-     *
-     * @param request  请求
-     * @param response 响应
-     */
-    @GetMapping("/.well-known/acme-challenge/*")
-    public void letUsEncryptCertificateCheck(HttpServletRequest request, HttpServletResponse response) {
-        try {
-            String uri = request.getRequestURI().replace("/", "\\");
-            //文件路径自行替换一下就行,就是上图中生成验证文件的路径,因为URI中已经包含了/.well-known/acme-challenge/,所以这里不需要
-            File file = new File(ZoneProperties.getCertificate().getPlace() + uri);
-            uploadService.download("验证文件", file, response, request);
-        } catch (Exception e) {
-            log.error("Let's encrypt certificate error : {} ", e);
-        }
     }
 }
