19e08d50a287965200aab5d2a6eb787eb206e9fa
zbeboy <863052317@qq.com>
2020-03-07 17:15:02 +0800

    优化

--- src/main/java/top/zbeboy/zone/service/notify/SystemNotifyServiceImpl.java
+++ src/main/java/top/zbeboy/zone/service/notify/SystemNotifyServiceImpl.java
@@ -2,13 +2,13 @@ package top.zbeboy.zone.service.notify;
 
 import org.jooq.DSLContext;
 import org.jooq.Result;
-import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import top.zbeboy.zone.domain.tables.records.SystemNotifyRecord;
 
+import static org.jooq.impl.DSL.now;
 import static top.zbeboy.zone.domain.Tables.SYSTEM_NOTIFY;
 
 @Service("systemNotifyService")
@@ -25,6 +25,8 @@ public class SystemNotifyServiceImpl implements SystemNotifyService {
     @Override
     public Result<SystemNotifyRecord> findByEffective() {
         return create.selectFrom(SYSTEM_NOTIFY)
-                .where(SYSTEM_NOTIFY.EXPIRE_DATE.ge(DSL.now())).fetch();
+                .where(SYSTEM_NOTIFY.EXPIRE_DATE.ge(now())
+                        .and(SYSTEM_NOTIFY.VALID_DATE.le(now()))
+                        .and(SYSTEM_NOTIFY.EXPIRE_DATE.gt(SYSTEM_NOTIFY.VALID_DATE))).fetch();
     }
 }
