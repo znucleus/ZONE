1dbabfa30634951b8e66bd4d56bead9e7d3b501a
zbeboy <863052317@qq.com>
2020-08-01 15:04:40 +0800

    疫情登录修复

--- src/main/java/top/zbeboy/zone/api/register/epidemic/RegisterEpidemicApiController.java
+++ src/main/java/top/zbeboy/zone/api/register/epidemic/RegisterEpidemicApiController.java
@@ -2,20 +2,18 @@ package top.zbeboy.zone.api.register.epidemic;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import top.zbeboy.zbase.config.Workbook;
 import top.zbeboy.zbase.domain.tables.pojos.Users;
 import top.zbeboy.zbase.feign.register.RegisterEpidemicService;
 import top.zbeboy.zbase.tools.web.util.AjaxUtil;
+import top.zbeboy.zbase.vo.register.epidemic.EpidemicRegisterDataAddVo;
 import top.zbeboy.zone.annotation.logging.ApiLoggingRecord;
 import top.zbeboy.zone.web.util.SessionUtil;
-import top.zbeboy.zbase.vo.register.epidemic.EpidemicRegisterDataAddVo;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 import java.security.Principal;
 import java.util.Map;
 
@@ -33,7 +31,7 @@ public class RegisterEpidemicApiController {
      */
     @ApiLoggingRecord(remark = "疫情登记数据保存", channel = Workbook.channel.API, needLogin = true)
     @PostMapping("/api/register/epidemic/data/save")
-    public ResponseEntity<Map<String, Object>> dataSave(@Valid EpidemicRegisterDataAddVo epidemicRegisterDataAddVo,
+    public ResponseEntity<Map<String, Object>> dataSave(EpidemicRegisterDataAddVo epidemicRegisterDataAddVo,
                                                         Principal principal, HttpServletRequest request) {
         Users users = SessionUtil.getUserFromOauth(principal);
         epidemicRegisterDataAddVo.setUsername(users.getUsername());
