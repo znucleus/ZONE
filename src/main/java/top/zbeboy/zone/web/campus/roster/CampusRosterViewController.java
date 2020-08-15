ce17bb03e11795fce73d97a2d15c629b864813da
zbeboy <863052317@qq.com>
2020-08-15 20:18:34 +0800

    开学权限调整

--- src/main/java/top/zbeboy/zone/web/campus/roster/CampusRosterViewController.java
+++ src/main/java/top/zbeboy/zone/web/campus/roster/CampusRosterViewController.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import top.zbeboy.zbase.domain.tables.pojos.Users;
+import top.zbeboy.zbase.feign.campus.roster.RosterReleaseService;
 import top.zbeboy.zone.web.util.SessionUtil;
+
+import javax.annotation.Resource;
 
 @Controller
 public class CampusRosterViewController {
+
+    @Resource
+    private RosterReleaseService rosterReleaseService;
 
     /**
      * 校园花名册
@@ -17,7 +23,7 @@ public class CampusRosterViewController {
     @GetMapping("/web/menu/campus/roster")
     public String index(ModelMap modelMap) {
         Users users = SessionUtil.getUserFromSession();
-//        modelMap.addAttribute("canRelease", schoolOpensService.opensConditionRelease(users.getUsername()));
+        modelMap.addAttribute("canRelease", rosterReleaseService.canRelease(users.getUsername()));
         return "web/campus/roster/roster_data::#page-wrapper";
     }
 }
