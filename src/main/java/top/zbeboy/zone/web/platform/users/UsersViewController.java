9627362cfeba30c52f78491126503a8c10b4d564
zbeboy <863052317@qq.com>
2021-02-12 18:21:20 +0800

    模糊化更改

--- src/main/java/top/zbeboy/zone/web/platform/users/UsersViewController.java
+++ src/main/java/top/zbeboy/zone/web/platform/users/UsersViewController.java
@@ -215,8 +215,8 @@ public class UsersViewController {
         Users users = SessionUtil.getUserFromSession();
         modelMap.addAttribute("username", users.getUsername());
         modelMap.addAttribute("email", StringUtils.overlay(users.getEmail(), "****", 1, users.getEmail().lastIndexOf("@")));
-        modelMap.addAttribute("mobile", StringUtils.overlay(users.getMobile(), "****", 3, 6));
-        modelMap.addAttribute("idCard", StringUtils.isNotBlank(users.getIdCard()) ? StringUtils.overlay(users.getIdCard(), "****", 3, users.getIdCard().length() - 2) : "");
+        modelMap.addAttribute("mobile", StringUtils.overlay(users.getMobile(), "****", 3, 7));
+        modelMap.addAttribute("idCard", StringUtils.isNotBlank(users.getIdCard()) ? StringUtils.overlay(users.getIdCard(), "****", 3, users.getIdCard().length() - 4) : "");
         modelMap.addAttribute("plaintextIdCard", users.getIdCard());
 
         Optional<GoogleOauth> optionalGoogleOauth = usersService.findGoogleOauthByUsername(users.getUsername());
